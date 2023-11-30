e20e3e4be98480ed9dfd290f63bcea98f8b7c5b1
lakithaomal <lhw150030@utdallas.edu>
2023-11-30 20:07:38 +0000

    audio updates

--- firmware/xu4Mqtt/runAll.sh
+++ firmware/xu4Mqtt/runAll.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
 sleep 55
-export LD_PRELOAD=/home/teamlary/.local/lib/python3.8/site-packages/scikit_learn.libs/libgomp-d22c30c5.so.1.0.0
-sleep 5
-
-kill $(pgrep -f 'python3 audioDeleter.py')
-sleep 5
-python3 audioDeleter.py &
-sleep 5
-
-kill $(pgrep -f 'python3 audioRecorder.py')
-sleep 5
-python3 audioRecorder.py &
-sleep 5
 
 kill $(pgrep -f 'ips7100ReaderV1.py')
 sleep 5
--- firmware/xu4Mqtt/runAllDeleter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sleep 30
-python3 audioDeleter.py &
-sleep 5
-python3 deleter.py &
-sleep 5
