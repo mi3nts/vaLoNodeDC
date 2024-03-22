2c1f6150387a0c1a068dbdfd91f8043bb0fcac40
lakihaomal <lhw150030@utdallas.edu>
2024-03-22 12:39:32 -0500

    minor updates

--- firmware/xu4Mqtt/rg15USBReader.py
+++ firmware/xu4Mqtt/rg15USBReader.py
@@ -6,7 +6,7 @@ import sys
 from mintsXU4 import mintsSensorReader as mSR
 from mintsXU4 import mintsDefinitions as mD
 
-rainPort               = mD.USBRG15Port
+rainPort               = mD.USBRG15Port[0]
 firstReset             = True
 firstResolutionSetup   = True
 firstMetricSystemSetup = True
